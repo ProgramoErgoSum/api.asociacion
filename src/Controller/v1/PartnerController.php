3ea17c30492b04b339469af6843ca779b4dd8600
Migue <migueabellan@gmail.com>
2018-08-01 08:52:51 +0200

    Add IndexController

--- src/Controller/v1/PartnerController.php
+++ src/Controller/v1/PartnerController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\v1;
 
 use App\Entity\Partner;
 use App\Entity\Subscription;
--- tests/Controller/v1/PartnerControllerTest.php
+++ tests/Controller/v1/PartnerControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace App\Tests\Controller\v1;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
